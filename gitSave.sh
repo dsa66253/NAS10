75513eb1a1b844ec181a7d72f9b2989292f1d53e
mary <dsa66253@yahoo.com.tw>
2022-09-13 10:09:38 +0800

    experiment0912_3

--- gitSave.sh
+++ gitSave.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git add .
-git commit -m "experiment0912"
+git commit -m "experiment0912_3"
--- models/initWeight.py
+++ models/initWeight.py
@@ -21,7 +21,7 @@ def initialize_weights(model):
             # torch.nn.init.uniform_(m.weight, -0.0125/2, 0.0125/2)
             # m.weight.data.fill_(0)
             # setTensorPositive(m.weight)
-            torch.nn.init.normal_(m.weight, 0.025, 0.025/2)
+            torch.nn.init.normal_(m.weight, 0.0125, 0.0125/2)
             if m.bias is not None:
                 torch.nn.init.constant_(m.bias, 1)
         elif isinstance(m, nn.Linear):
@@ -30,7 +30,7 @@ def initialize_weights(model):
             # torch.nn.init.uniform_(m.weight, -0.0125/2, 0.0125/2)
             # m.weight.data.fill_(0)
             # nn.init.constant_(m.bias, 0)
-            torch.nn.init.normal_(m.weight, 0.02, 0.02/2)
+            torch.nn.init.normal_(m.weight, 0.0125, 0.0125/2)
             pass
         elif isinstance(m, nn.BatchNorm2d):
             if m.weight is not None:
